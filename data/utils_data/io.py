2ac7ad1ea378da1aa1acc3c58b23334d1f611826
Hubert Kanyamahanga <kanyamahanga.hubert@aims-senegal.org>
2026-09-25 19:41:43 +0200

    Updated

--- data/utils_data/io.py
+++ data/utils_data/io.py
@@ -2,13 +2,13 @@ import os
 import rasterio
 import numpy as np
 
-DATA_DIR = "D:\kanyamahanga\Datasets"
+# DATA_DIR = "D:\kanyamahanga\Datasets"
 
-# try:
-#     DATA_DIR = "/hubert_storage"
-#     # DATA_DIR = "/hubert_storage"
-# except Exception:
-#     DATA_DIR = "D:\kanyamahanga\Datasets"
+try:
+    DATA_DIR = "/hubert_storage"
+    # DATA_DIR = "/hubert_storage"
+except Exception:
+    DATA_DIR = "D:\kanyamahanga\Datasets"
 
 
 def read_patch(raster_file: str, channels: list = None) -> np.ndarray:
